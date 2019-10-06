cac578b8c2ac95b37daf65e6a9f776d4957db732
Syaqinah Safar <syaqinahsafar23@gmail.com>
2019-10-06 20:41:39 +0800

    made a change in file index.php

--- app/controllers/About.php
+++ app/controllers/About.php
@@ -1,9 +1,13 @@
 <?php
 
-class About {
+class About extends Controller {
 	public function index($name = 'Syaqinah', $status = 'Student', $age = 22)
 	{
-		$this->view('about/index');
+		$data['name'] = $name;
+		$data['status'] = $status;
+		$data['age'] = $age;
+		
+		$this->view('about/index', $data);
 	}
 	public function page()
 	{
