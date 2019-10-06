df48c25e911d4f6b01cd85be12f9b205f369eab4
Syaqinah Safar <syaqinahsafar23@gmail.com>
2019-10-06 20:42:45 +0800

    made a change

--- app/views/about/index.php
+++ app/views/about/index.php
@@ -1,11 +1,13 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
 	<meta charset="utf-8">
 	<title>Halaman About</title>
+	<link rel="stylesheet" href="">
 </head>
 <body>
 	<h1>About Me</h1>
+	<p>Hye, My name <?= $data['name']; ?>, i am <?= $data['age']; ?> years old and i am a <?= $data['status']; ?>. </p>
 
 </body>
 </html>
