3bde91bed5dc92726a3bae745d73d1eb8b3bdcc3
Syaqinah Safar <syaqinahsafar23@gmail.com>
2019-10-06 21:10:38 +0800

    made a change in file About.php

--- app/controllers/About.php
+++ app/controllers/About.php
@@ -6,11 +6,18 @@ class About extends Controller {
 		$data['name'] = $name;
 		$data['status'] = $status;
 		$data['age'] = $age;
-		
+		$data['title'] = 'About Me';
+
+		$this->view('templates/header', $data);
 		$this->view('about/index', $data);
+		$this->view('templates/footer');
 	}
 	public function page()
 	{
+		$data['title'] = 'Pages';
+
+		$this->view('templates/header', $data);
 		$this->view('about/page');
+		$this->view('templates/footer');
 	}
 }
