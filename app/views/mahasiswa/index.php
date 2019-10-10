f5f31bbb9374f7b77d4275e54829e4944d2a9d23
Syaqinah Safar <syaqinahsafar23@gmail.com>
2019-10-11 00:21:11 +0800

    add and modified file and also folder

--- app/views/mahasiswa/index.php
+++ app/views/mahasiswa/index.php
@@ -3,14 +3,16 @@
 	<div class="row">
 		<div class="col-6">
 			<h3>Daftar Mahasiswa</h3>
-				<?php  foreach ($data['mhs'] as $mhs ) : ?>
-				 <ul>
-				 	<li><?= $mhs['name'];  ?></li>
-				 	<li><?= $mhs['matric'];  ?></li>
-				 	<li><?= $mhs['email'];  ?></li>
-				 	<li><?= $mhs['course'];  ?></li>
-				 </ul>
-				<?php endforeach; ?>	
+			<ul class="list-group">
+				  <?php  foreach ($data['mhs'] as $mhs ) : ?>
+				  <li class="list-group-item d-flex justify-content-between align-items-center">
+				  	<?= $mhs['name']; ?>
+				  	<a href="<?= BASEURL; ?>/mahasiswa/detail/<?= $mhs['id']; ?>" class="badge badge-dark">detail</a>
+				  		
+				  </li>
+				  <?php endforeach; ?>	
+			</ul>
+						
 		</div>
 	</div>
 
